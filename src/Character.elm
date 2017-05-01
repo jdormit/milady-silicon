1f3c5eacac569c02673a4ec8a57ce6d8b6ce87f2
Jeremy Dormitzer <jeremy.dormitzer@gmail.com>
2017-05-01 15:46:28 -0600

    Add character type

---
+++ src/Character.elm
@@ -0,0 +1,11 @@
+module Character exposing (Character)
+
+
+type alias Character =
+    { firstName : String
+    , lastName : String
+    , gender : String
+    , descriptor : String
+    , role : String
+    , distinguishingFeature : String
+    }
